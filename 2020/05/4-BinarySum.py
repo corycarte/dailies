3e42de89c6121be315f93b0e6dada81e4cd67d6b
Cory Carte <cory.carte@gmail.com>
2020-05-04 11:07:02 -0500

    Added problem for 2020-05-04

---
+++ 2020/05/4-BinarySum.py
@@ -0,0 +1,18 @@
+# Daily Interview Pro - 04-26-2020
+# Problem - Given two binary numbers represented as strings,
+#           return the sum of the two binary numbers as a new binary represented as a string.
+#           Do this without converting the whole binary string into an integer.
+
+def sum_binary(bin1, bin2):
+    # complete function here
+    # personal challenge, give recursive solution
+    print('start')
+
+
+# Test Code
+print(sum_binary('11101', '1011'))
+# 10100
+print(sum_binary('00000', '1111'))
+# 1111
+print(sum_binary('', '1011'))
+# 1011
